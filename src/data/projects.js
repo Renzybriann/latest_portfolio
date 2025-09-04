8e3d73901302f010afae171036536958af708abc
Renzybriann <youngrenzybrian@gmail.com>
2025-09-04 15:16:35 +0800

    Add HR.png to public folder

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,22 @@
 export const projectsData = [
   {
     title: "HRM - Human Resource Management System",
-    description: "Problem: The organization's outdated HR management system caused delays in employee onboarding and lacked user access control, leading to inefficiencies and data security concerns. Action: I led the front-end development of a new HR management platform using React, Tailwind CSS, and PostgreSQL, with role-based access for Admin, HR Officers, and Users. Result: The new system reduced onboarding time by 40%, improved data security through user type restrictions, and increased HR team productivity.",
-    imageUrl: "https://osbozdkotesdrlrwuxwa.supabase.co/storage/v1/object/public/portfolio//HRM3.png",
-    techStack: ['Laravel', 'Supabase', 'Render', 'Tailwind CSS', 'PostgreSQL'],
+    description:
+      "Problem: The organization's outdated HR management system caused delays in employee onboarding and lacked user access control, leading to inefficiencies and data security concerns. Action: I led the front-end development of a new HR management platform using React, Tailwind CSS, and PostgreSQL, with role-based access for Admin, HR Officers, and Users. Result: The new system reduced onboarding time by 40%, improved data security through user type restrictions, and increased HR team productivity.",
+    imageUrl:
+      "https://raw.githubusercontent.com/Renzybriann/latest_portfolio/main/public/HR.png",
+    techStack: ["Laravel", "Supabase", "Render", "Tailwind CSS", "PostgreSQL"],
     liveUrl: "https://hrm-system-p91x.onrender.com/",
-    repoUrl: "https://github.com/Renzybriann/hrm-system.git"
+    repoUrl: "https://github.com/Renzybriann/hrm-system.git",
   },
   {
     title: "Work in Greenland - Specialized Recruitment & Consultation",
-    description: "Problem: Companies struggled to find and retain skilled professionals prepared for Greenland's unique work environment and complex labor regulations. Action: I built a recruitment platform using React, Vite, and Tailwind CSS, integrating Formspree for inquiries and Sanity for content management to streamline the application process. Result: The website successfully connected businesses with a dedicated pool of qualified candidates, reducing hiring times and improving employee retention across Greenland.",
-    imageUrl: "https://osbozdkotesdrlrwuxwa.supabase.co/storage/v1/object/public/portfolio//wgl1.png",
-    techStack: ['Vite', 'Tailwind CSS', 'Formspree', 'Sanity'],
+    description:
+      "Problem: Companies struggled to find and retain skilled professionals prepared for Greenland's unique work environment and complex labor regulations. Action: I built a recruitment platform using React, Vite, and Tailwind CSS, integrating Formspree for inquiries and Sanity for content management to streamline the application process. Result: The website successfully connected businesses with a dedicated pool of qualified candidates, reducing hiring times and improving employee retention across Greenland.",
+    imageUrl:
+      "https://raw.githubusercontent.com/Renzybriann/latest_portfolio/main/public/WGL.png",
+    techStack: ["Vite", "Tailwind CSS", "Formspree", "Sanity"],
     liveUrl: "https://work-in-gl.vercel.app/",
-    repoUrl: "https://github.com/Renzybriann/manpower-website"
-  }
+    repoUrl: "https://github.com/Renzybriann/manpower-website",
+  },
 ];
